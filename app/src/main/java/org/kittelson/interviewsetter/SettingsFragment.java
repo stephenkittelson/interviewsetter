0ee46b0a411c0018d5560798e4236bf313e2fbe1
Stephen Kittelson <stephen.kittelson@gmail.com>
2019-09-15 16:09:14 -0600

    Continuing setup of settings activity.

---
+++ app/src/main/java/org/kittelson/interviewsetter/SettingsFragment.java
@@ -0,0 +1,13 @@
+package org.kittelson.interviewsetter;
+
+import android.os.Bundle;
+
+import androidx.preference.PreferenceFragmentCompat;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.pref_general, rootKey);
+    }
+
+}

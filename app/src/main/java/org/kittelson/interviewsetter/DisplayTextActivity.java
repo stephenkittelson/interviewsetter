30d9076bcb5a52f5027197b7cac6c301c0976664
Stephen Kittelson <stephen.kittelson@gmail.com>
2020-04-12 12:20:24 -0600

    Fixing coloring of title in DisplayText activity.

--- app/src/main/java/org/kittelson/interviewsetter/DisplayTextActivity.java
+++ app/src/main/java/org/kittelson/interviewsetter/DisplayTextActivity.java
@@ -1,16 +1,11 @@
 package org.kittelson.interviewsetter;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+import android.text.method.ScrollingMovementMethod;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.text.method.ScrollingMovementMethod;
-import android.view.View;
-import android.widget.TextView;
 
 public class DisplayTextActivity extends AppCompatActivity {
     public static final String CONTENT_KEY = "content";
